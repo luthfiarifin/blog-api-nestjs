654260b4f7681dd616e6a3c495876dbd02eb0b11
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-12-15 21:25:50 +0700

    Inject user module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseConnectionService } from './database-connection.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useClass: DatabaseConnectionService,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
